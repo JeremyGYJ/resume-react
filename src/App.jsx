7007d8738bfb8ad2e8609984010db38897e0329b
JeremyGYJ <ganyewjoo.jeremy@gmail.com>
2024-01-03 22:10:34 +0800

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Jeremy Gan</h1>
+    <h2>Software Developer</h2>
+    </>
+  )
+}
+
+export default App;
